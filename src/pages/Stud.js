ffab5105dbd549d4df95e5b6c598ef7dedae74e1
Hemanth Pothu <56197063+Hemanthpothu@users.noreply.github.com>
2023-04-30 11:57:39 +0530

    2 folders added

---
+++ src/pages/Stud.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../components/footer/Footer";
+import Title from "../components/layouts/Title";
+import Navbar from "../components/navbar/Navbar";
+import App1 from "./student/src/App.js";
+
+function Student() {
+    return (
+      <div>
+        {/* <Navbar /> */}
+        <App1/>
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+  
+  export default Student;
